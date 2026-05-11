47138bd5d3908d5502b1dd9696f5a34979055926
Amritha T <amrithathanur@gmail.com>
2026-05-11 14:14:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.js
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.js
@@ -3,9 +3,5 @@
  * @return {number[]}
  */
 var separateDigits = function(nums) {
-    let res=[]
-    for (let i=0;i<nums.length;i++){
-         res.push(...nums[i].toString().split(""))
-    }
-    return res.map(a => Number(a))
+    return nums.join("").split("").map(Number)
 };
